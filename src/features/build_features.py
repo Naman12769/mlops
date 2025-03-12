95eedf6e8c72873f30fe1a0882730dab7d8a18b3
Naman12769 <namanjainjain341@gmail.com>
2025-03-12 14:43:55 +0530

    files added due to the removal of data folder

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,2 +1,4 @@
 def create_feature(data):
   return data
+# in notebooks data is in interactive manner and all of the data in single file in notebook like model info,datasets info and many more.
+# but in src files code in not appropriate manner because all of the required data exist on same file it is not necessary. let's have a example. if i want to a datasets information in feature folder then we want to export datasets information from data folder.
